762621858ce34c51c5c9bbff154d335e85e9f355
Edras <monterrosoedras@gmail.com>
2024-07-25 22:05:57 -0600

    fix null values

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const suma = (valor1, valor2) => {
-  if (isNaN(valor1) || isNaN(valor2))
+  if (isNaN(valor1) || isNaN(valor2) || valor1 === null || valor2 === null)
     return "El tipo del parámetro tiene que ser numérico.";
   return valor1 + valor2;
 };
 
 const resta = (valor1, valor2) => {
-  if (isNaN(valor1) || isNaN(valor2))
+  if (isNaN(valor1) || isNaN(valor2) || valor1 === null || valor2 === null)
     return "El tipo del parámetro tiene que ser numérico.";
   return valor1 - valor2;
 };
 
 const division = (valor1, valor2) => {
-  if (isNaN(valor1) || isNaN(valor2))
+  if (isNaN(valor1) || isNaN(valor2) || valor1 === null || valor2 === null)
     return "El tipo del parámetro tiene que ser numérico.";
 
   if (valor2 === 0) return "No se puede divir por cero";
@@ -19,7 +19,7 @@ const division = (valor1, valor2) => {
 };
 
 const multi = (valor1, valor2) => {
-  if (isNaN(valor1) || isNaN(valor2))
+  if (isNaN(valor1) || isNaN(valor2) || valor1 === null || valor2 === null)
     return "El tipo del parámetro tiene que ser numérico.";
 
   return valor1 * valor2;
